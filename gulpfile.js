a9a962751b820d3727ddf815714a4b001b7c5b44
dotch <cweiss@mozilla.com>
2014-05-27 11:14:40 -0700

    added gulp task for styles and server, renamed files to avoid multiple index.html, removed vulcanize for now, updated paths where necessary

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,41 @@
 /* jshint node:true */
 var gulp = require('gulp');
 
-var jshint = require('gulp-jshint');
-var vulcanize = require('gulp-vulcanize');
+var jshint = require('gulp-jshint'),
+    connect = require('gulp-connect'),
+    jshint = require('gulp-jshint'),
+    stylus = require('gulp-stylus'),
+    concat = require('gulp-concat');
 
 var paths = {
-  'main': 'src/index.html',
+  'main': 'src/element.html',
   'scripts': 'src/*.js',
-  'styles': 'src/*.styl'
+  'stylesheets': 'src/*.styl',
+  'gulpfile': 'gulpfile.js'
 };
 
-gulp.task('build', function() {
-  gulp.src(paths.main)
-      .pipe(vulcanize({
-        dest: 'dist'
-      }))
-      .pipe(gulp.dest('dist'));
-});
-
 gulp.task('lint', function() {
-  return gulp.src(paths.scripts)
+  return gulp.src([paths.scripts, paths.gulpfile])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('watch', function () {
-  gulp.watch(paths.scripts, ['lint', 'build']);
+gulp.task('styles', function() {
+  gulp.src('src/*.styl')
+    .pipe(stylus())
+    .pipe(concat('element.css'))
+    .pipe(gulp.dest('src'));
 });
+
+gulp.task('connect', function() {
+  connect.server();
+});
+
+gulp.task('watch', function () {
+  gulp.watch([paths.scripts, paths.gulpfile], ['lint']);
+  gulp.watch(paths.stylesheets, ['styles']);
+});
+
+gulp.task('build', ['lint','styles']);
+gulp.task('server', ['lint','styles','connect','watch']);
+
