b7fd0a8da388e85c4220f507652bc9204d68f16c
dotch <cweiss@mozilla.com>
2014-06-12 14:05:29 -0700

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -53,14 +53,12 @@ gulp.task('server', ['build','connect','watch']);
 
 // run the tests
 gulp.task('test', function() {
-  // Be sure to return the stream
   return gulp.src(paths.testfiles)
     .pipe(karma({
       configFile: 'karma.conf.js',
       action: 'run'
     }))
     .on('error', function(err) {
-      // Make sure failed tests cause gulp to exit non-zero
       throw err;
     });
 });
